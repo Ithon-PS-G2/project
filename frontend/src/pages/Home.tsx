f7eae8ea4fc10f6081da1efa1d0edb81858bffab
Gabriel Henrique Ferreira Borges <gabrielhfborges6@gmail.com>
2026-05-10 11:32:23 -0300

    Tailwindcss and Routes Setup

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,7 @@
+export function Home(){
+  return(
+    <main className="flex justify-center items-center h-screen">
+      <h1 className="text-4xl font-bold text-green-500">Hello World!</h1>
+    </main>
+  )
+}
